850f31ff78445c7c5dedfef965dcdb478793ad06
acodered <alexkondratru@gmail.com>
2023-10-29 18:32:36 +0100

    Implement  function

--- main.py
+++ main.py
@@ -24,12 +24,31 @@ def movingAverage(signal: np.array, h: int) -> np.array:
         filtered_signal[i] = 1/h * np.sum(signal[i:h+i])
     return filtered_signal
 
+def MSE(first: np.array, second: np.array) -> float:
+    """"
+        Calculates the Mean Squared Error of two passed arrays.
+        
+        Arguments:
+        first: np.array - first array
+        second: np.array - second array
+        
+        Returns:
+        float - MSE of two passed arrays
+    """
+    if first.size != second.size:
+        raise ValueError(f"Number of samples in first and second arrays must match! (first={(first.size)}, second={(second.size)})")
+    mse = np.sum(np.square(first-second)) / first.size
+    return mse    
+    
+
 h = 30
 t = np.linspace(0, 2*np.pi, 628)
 function = np.sin(t)
 noise = np.random.randn(t.size) / 10
 noised_function = function + noise
 filtered = movingAverage(noised_function, h)
+
+print(f"MSE = {MSE(function[h:], filtered)}")
 
 plt.plot(t, noised_function)
 plt.plot(t, function)
