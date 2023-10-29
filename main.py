9284353d72105540f543681d43e7c9e5abbc8b96
acodered <alexkondratru@gmail.com>
2023-10-29 18:21:42 +0100

    Implement  function

--- main.py
+++ main.py
@@ -18,14 +18,20 @@ def movingAverage(signal: np.array, h: int) -> np.array:
         Returns:
         np.array - filtered signal of size `(n-h)` 
     """
+    
+    filtered_signal = np.zeros(signal.size - h)
+    for i in range(filtered_signal.size):
+        filtered_signal[i] = 1/h * np.sum(signal[i:h+i])
+    return filtered_signal
 
+h = 30
 t = np.linspace(0, 2*np.pi, 628)
 function = np.sin(t)
 noise = np.random.randn(t.size) / 10
 noised_function = function + noise
-
-
+filtered = movingAverage(noised_function, h)
 
 plt.plot(t, noised_function)
 plt.plot(t, function)
+plt.plot(t[h:], filtered)
 plt.show()
