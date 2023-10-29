fc40e0a2309538680878d8afc1497dbd6e74b422
acodered <alexkondratru@gmail.com>
2023-10-29 18:14:15 +0100

    Declare  function

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+def movingAverage(signal: np.array, h: int) -> np.array:
+    """
+        Simple moving average filter.
+        
+        Moving average filter made for smoothing signal's noise.
+        !Important!
+        This function cuts `h` first samples of the `signal`. For 
+        input `signal` of size `(1, 628)` output signal will have 
+        size `(1, 628-h)` and first `h` samples will be lost.
+        
+        Arguments:
+        signal: np.array - signal to filter
+        h: int - number of samples to consider
+        
+        Returns:
+        np.array - filtered signal of size `(n-h)` 
+    """
+
 t = np.linspace(0, 2*np.pi, 628)
 function = np.sin(t)
 noise = np.random.randn(t.size) / 10
 noised_function = function + noise
+
+
 
 plt.plot(t, noised_function)
 plt.plot(t, function)
