b2494554b1f8676dc1ad1b36f06ceaae3d615d4d
acodered <alexkondratru@gmail.com>
2023-10-30 17:53:07 +0100

    Prepare everything for plots creation

--- main.py
+++ main.py
@@ -36,42 +36,79 @@ def MSE(first: np.array, second: np.array) -> float:
         float - MSE of two passed arrays
     """
     if first.size != second.size:
-        raise ValueError(f"Number of samples in first and second arrays must match! (first={(first.size)}, second={(second.size)})")
+        raise ValueError(f"Number of samples in first and second \
+            arrays must match! (first={(first.size)}, second={(second.size)})")
     return np.mean(np.square(first-second))
     
 
-noise_scale = 0.15
-h_array = np.arange(1, 21, 1)  
+"""
+    Main function variables:
+    
+    h_array: np.array - array of 'h' values to loop through
+    t: np.array - array of original function argument values
+    function: np.array - array of original function values
+"""
+h_min = 1
+h_max = 21
+h_array = np.arange(h_min, h_max, 1) 
 t = np.linspace(0, 2*np.pi, 1000)
 function = np.sin(t)
 
+"""
+    Sampling variables:
+    
+    n_samples: int - number of samples. 
+               Must be such that: 1000 % n_samples = 0;
+    samples_t: np.array - array of time value for each sample
+    samples_array: np.array - array of values of original function 
+                              for each samples_t value
+    noise: np.array - array of random noise values from normal distribution
+    noise_scale: float (0, +inf] - value for scailing noise applied
+                                   to each sample
+    noised_samples: np.array - array of sample values with noise
+"""
+noise_scale = 0.15
 n_samples = 250
-samples_scale = int(t.size / n_samples)
-samples_array = np.sin(t[::samples_scale])
-
-
+samples_t = np.linspace(0, 2*np.pi, n_samples)
+samples_array = np.sin(samples_t)
 noise = noise_scale * np.random.randn(n_samples) 
-noised_function = samples_array + noise
-mse_array = np.zeros(h_array.size)
+noised_samples = samples_array + noise
+
+mse_array = np.zeros(h_array.size) # MSE values for each 'h' in 'h_array'
 
 for h in h_array:
-    filtered = movingAverage(noised_function, h)
-    mse_array[h-1] = MSE(filtered, function[h*samples_scale::samples_scale])
+    filtered = movingAverage(noised_samples, h)
+    mse_array[h-1] = MSE(filtered, samples_array[h:])
 
-# h = 7
-# filtered = movingAverage(samples_array, h)
-# print(f"FILTERED SIZE : {filtered.size}")
-# print
 
-# plt.scatter(t[::samples_scale], noised_function)
-# plt.plot(t, function)
-# plt.plot(t[h:], filtered)
 min_mse = np.argmin(mse_array)
-print(f"MIN MSE = {mse_array[min_mse]}, h = {h_array[min_mse]}")
+h = h_array[min_mse]
+mse = mse_array[min_mse]
+print(f"MIN MSE = {mse}, h = {h}")
 
+filtered = movingAverage(noised_samples, h)
 
-plt.scatter(t[::samples_scale], noised_function, color="red", marker='.')
-plt.plot(t, function)
-plt.plot(t[5*samples_scale::samples_scale], movingAverage(noised_function, 5)) 
-# plt.plot(h_array, mse_array)
+plt.figure(figsize=(8, 4))
+plt.plot(t, function,
+         label="Funkcja oryginalna")
+plt.plot(samples_t[h:], filtered, '--g',
+         label=f"Pomiary uśrednione, h={h}")
+plt.scatter(samples_t, noised_samples, 
+            color="red", 
+            marker='.',
+            label=f"Pomiary zaszumione, noise_scale={noise_scale}")
+plt.yticks(np.arange(-1.25, 1.25, 0.25))
+plt.xticks(np.arange(0.0, 6.5, 0.5))
+plt.xlabel("t")
+plt.ylabel("f(t)")
+plt.grid()
+plt.legend()
+
+plt.figure(figsize=(8, 4))
+plt.plot(h_array, mse_array, '--o')
+plt.title("Zależność MSE od wartośći h")
+plt.xlabel("h")
+plt.ylabel("MSE(h)")
+plt.xticks(np.arange(0, h_max, 1))
+plt.grid()
 plt.show()
